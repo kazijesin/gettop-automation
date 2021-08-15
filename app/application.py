e1f4976a041213747895a3a9a2227f356296dc25
kazijesin <kazijesin@gmail.com>
2021-08-15 12:34:17 -0400

    updating file

---
+++ app/application.py
@@ -0,0 +1,20 @@
+from pages.header import Header
+from pages.main_page import Main
+from pages.product_page import ProductPage
+from pages.search_result_page import SearchResults
+from pages.top_banners import TopBanners
+
+
+
+
+class Application:
+
+    def __init__(self,driver):
+     self.driver = driver
+
+     self.main_page = Main(self.driver)
+     self.header = Header(self.driver)
+     self.product_page = ProductPage(self.driver)
+     self.search_result_page = SearchResults(self.driver)
+     self.top_banners = TopBanners(self.driver)
+
